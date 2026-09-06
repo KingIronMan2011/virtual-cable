465eaf420f387bc9b0cda87f48607ae9e9e63957
Julian Speckmann <176780813+KingIronMan2011@users.noreply.github.com>
2026-09-06 15:51:00 +0200

    chore: update package.json to specify Node.js and pnpm versions

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,18 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { readFileSync } from "fs";
-import { resolve } from "pathe";
-
-const rootPackageJson = JSON.parse(
-  readFileSync(resolve(__dirname, "../package.json"), "utf-8"),
-);
 
 export default defineConfig({
   base: "/",
   plugins: [react()],
-  define: {
-    __APP_VERSION__: JSON.stringify(rootPackageJson.version),
-  },
   server: {
     port: 5173,
   },
